033363059bde0aa49b7e1fc5458fe7c311d856ab
izilzty <izilzty@outlook.com>
2020-09-20 18:05:35 +0800

    增加实时时钟驱动

--- Src/USER/func.c
+++ Src/USER/func.c
@@ -1,44 +1,78 @@
 #include "func.h"
 
-void Init(void)
+uint8_t ResetInfo;
+
+static void FirstInit(void) /* 系统首次上电、时钟数据丢失、按下了复位按键 初始化 */
 {
-    LL_EXTI_DisableIT_0_31(EPD_BUSY_EXTI0_EXTI_IRQn);                /* 禁用唤醒中断 */
-    LL_SYSCFG_VREFINT_SetConnection(LL_SYSCFG_VREFINT_CONNECT_NONE); /* 禁用VREFINT输出 */
-    LL_mDelay(199);
-
-    while (LL_GPIO_IsInputPinSet(BTN_SET_GPIO_Port, BTN_SET_Pin) == 0)
-    {
-        LL_mDelay(0);
-    }
-
-    switch (LP_GetResetInfo())
-    {
-    case LP_RESET_POWERON:
-        USART_SendStringRN("POWER ON");
-        LL_GPIO_ResetOutputPin(SHT30_POWER_GPIO_Port, SHT30_POWER_Pin);
-        LL_GPIO_SetOutputPin(I2C1_PULLUP_GPIO_Port, I2C1_PULLUP_Pin);
-        LL_mDelay(0);
-        I2C_Start(0xD0, 2, 0);
-        I2C_WriteByte(0x0E);
-        I2C_WriteByte(0x1C);
-        I2C_Stop();
-        LL_GPIO_ResetOutputPin(I2C1_PULLUP_GPIO_Port, I2C1_PULLUP_Pin);
-        LL_GPIO_SetOutputPin(SHT30_POWER_GPIO_Port, SHT30_POWER_Pin);
-        USART_SendStringRN("DS1302 RESET DONE");
-        break;
-    case LP_RESET_NORMALRESET:
-        USART_SendStringRN("RESET");
-        break;
-    case LP_RESET_WKUPSTANDBY:
-        USART_SendStringRN("EXIT STANDBY");
-        break;
-    }
-    USART_DebugPrint("In Standby");
-    LP_EnterStandby();
+    LL_GPIO_ResetOutputPin(SHT30_POWER_GPIO_Port, SHT30_POWER_Pin);
+    LL_GPIO_SetOutputPin(I2C1_PULLUP_GPIO_Port, I2C1_PULLUP_Pin);
+    LL_mDelay(0);
+    I2C_Start(0xD0, 2, 0);
+    I2C_WriteByte(0x0E);
+    I2C_WriteByte(0x1C);
+    I2C_Stop();
+    LL_GPIO_ResetOutputPin(I2C1_PULLUP_GPIO_Port, I2C1_PULLUP_Pin);
+    LL_GPIO_SetOutputPin(SHT30_POWER_GPIO_Port, SHT30_POWER_Pin);
 }
 
-void Loop(void)
+static void UpdateDisplay(void) /* 系统由实时时钟从Standby模式唤醒 */
 {
-    USART_SendStringRN("loop...");
-    LL_mDelay(999);
+    LL_mDelay(0); /* 唤醒按键消抖 */
+    while (LL_GPIO_IsInputPinSet(BTN_SET_GPIO_Port, BTN_SET_Pin) == 0)
+    {
+        LL_mDelay(9); /* 唤醒按键消抖 */
+    }
+}
+
+static void OpenMenu(void) /* 系统由“设置”按键从Standby模式唤醒 */
+{
+    LL_mDelay(0); /* 唤醒按键消抖 */
+    while (LL_GPIO_IsInputPinSet(BTN_SET_GPIO_Port, BTN_SET_Pin) == 0)
+    {
+        LL_mDelay(9); /* 唤醒按键消抖 */
+    }
+}
+
+void Init(void) /* 系统复位后执行一次 */
+{
+    USART_DebugPrint("SYSTEM RESET");
+    LL_EXTI_DisableIT_0_31(EPD_BUSY_EXTI0_EXTI_IRQn);                /* 禁用唤醒中断 */
+    LL_SYSCFG_VREFINT_SetConnection(LL_SYSCFG_VREFINT_CONNECT_NONE); /* 禁用VREFINT输出 */
+    ResetInfo = LP_GetResetInfo();
+    switch (ResetInfo)
+    {
+    case LP_RESET_POWERON:
+        USART_DebugPrint("POR reset");
+        break;
+    case LP_RESET_NORMALRESET:
+        USART_DebugPrint("Normal reset");
+        break;
+    case LP_RESET_WKUPSTANDBY:
+        USART_DebugPrint("Wakeup from standby");
+        break;
+    }
+}
+
+void Loop(void) /* 在Init()执行完成后循环执行 */
+{
+    switch (ResetInfo)
+    {
+    case LP_RESET_POWERON:
+        FirstInit();
+        break;
+    case LP_RESET_NORMALRESET:
+        FirstInit();
+        break;
+    case LP_RESET_WKUPSTANDBY:
+        UpdateDisplay();
+        break;
+    }
+
+    USART_DebugPrint("Wait in standby mode");
+    LP_EnterStandby(); /* 程序停止，等待下一次唤醒复位 */
+
+    /* 正常情况下程序会停止在此处 */
+
+    USART_DebugPrint("In standby mode fail");
+    NVIC_SystemReset(); /* 未成功进入Standby模式，手动软复位 */
 }
---
+++ Src/USER/rtc.h
@@ -0,0 +1,6 @@
+#ifndef _RTC_H_
+#define _RTC_H_
+
+#include "main.h"
+
+#endif
