fce1430ce570d05eee28ac171a0dcdce713326f6
izilzty <izilzty@outlook.com>
2020-09-29 20:40:31 +0800

    蜂鸣器修复

--- Src/USER/buzzer.c
+++ Src/USER/buzzer.c
@@ -7,7 +7,7 @@
 void BUZZER_Enable(void)
 {
     LL_TIM_CC_DisableChannel(BUZZER_TIMER, BUZZER_CHANNEL);
-    LL_TIM_SetCounter(BUZZER_TIMER, 0);
+    LL_TIM_SetCounter(BUZZER_TIMER, LL_TIM_GetAutoReload(BUZZER_TIMER) / 2);
     LL_TIM_EnableCounter(BUZZER_TIMER);
 }
 
@@ -18,7 +18,6 @@ void BUZZER_Disable(void)
 {
     LL_TIM_CC_DisableChannel(BUZZER_TIMER, BUZZER_CHANNEL);
     LL_TIM_DisableCounter(BUZZER_TIMER);
-    LL_TIM_SetCounter(BUZZER_TIMER, 0);
 }
 
 /**
@@ -81,6 +80,7 @@ void BUZZER_Beep(uint16_t time_ms)
  */
 void BUZZER_Start(void)
 {
+    LL_TIM_SetCounter(BUZZER_TIMER, LL_TIM_GetAutoReload(BUZZER_TIMER) / 2);
     LL_TIM_CC_EnableChannel(BUZZER_TIMER, BUZZER_CHANNEL);
 }
 
