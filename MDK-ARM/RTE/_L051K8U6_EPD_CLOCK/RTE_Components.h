1fa5be8747789dca06e440661370cc73f99593d1
izilzty <izilzty@outlook.com>
2021-01-19 00:18:38 +0800

    添加缺失字模：“蜡”

--- MDK-ARM/RTE/_L051K8U6_EPD_CLOCK/RTE_Components.h
+++ MDK-ARM/RTE/_L051K8U6_EPD_CLOCK/RTE_Components.h
@@ -1,21 +1,21 @@
-
-/*
- * Auto generated Run-Time-Environment Configuration File
- *      *** Do not modify ! ***
- *
- * Project: 'L051K8U6_EPD_CLOCK' 
- * Target:  'L051K8U6_EPD_CLOCK' 
- */
-
-#ifndef RTE_COMPONENTS_H
-#define RTE_COMPONENTS_H
-
-
-/*
- * Define the Device Header File: 
- */
-#define CMSIS_device_header "stm32l0xx.h"
-
-
-
-#endif /* RTE_COMPONENTS_H */
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'L051K8U6_EPD_CLOCK' 
+ * Target:  'L051K8U6_EPD_CLOCK' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32l0xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
