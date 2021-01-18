809517437282ce9184ec93ba1b8cbb82c0425dfe
izilzty <izilzty@outlook.com>
2021-01-18 22:58:48 +0800

    清理Keil中间文件

--- MDK-ARM/L051K8U6_EPD_CLOCK/system_stm32l0xx.d
+++
@@ -1,9 +0,0 @@
-l051k8u6_epd_clock/system_stm32l0xx.o: ..\Src\system_stm32l0xx.c \
-  ..\Drivers\CMSIS\Device\ST\STM32L0xx\Include\stm32l0xx.h \
-  ..\Drivers\CMSIS\Device\ST\STM32L0xx\Include\stm32l051xx.h \
-  ..\Drivers\CMSIS\Include\core_cm0plus.h \
-  D:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  ..\Drivers\CMSIS\Include\core_cmInstr.h \
-  ..\Drivers\CMSIS\Include\cmsis_armcc_V6.h \
-  ..\Drivers\CMSIS\Include\core_cmFunc.h \
-  ..\Drivers\CMSIS\Device\ST\STM32L0xx\Include\system_stm32l0xx.h
